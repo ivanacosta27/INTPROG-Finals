fd08e706f24bf7178ead8a120365e4d1ea4476ea
ivana <ivanacosta0627@gmail.com>
2024-05-16 08:10:37 +0800

    commit

--- buwisbuddyph/app/src/main/java/com/mab/buwisbuddyph/dataclass/new_Message.kt
+++ buwisbuddyph/app/src/main/java/com/mab/buwisbuddyph/dataclass/new_Message.kt
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 package com.mab.buwisbuddyph.dataclass
-=======
-package com.mab.buwisbuddyph.model
->>>>>>> 06eba06f8f144f744a001cb4713e07cdaf4a620a
 
 data class new_Message(
     val avatarUrl: String, // URL of the avatar image
