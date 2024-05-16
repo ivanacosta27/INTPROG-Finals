d724332f1bdec7b62092a2e21f87b00598e82a03
Ivan Acosta <140309833+ivanacosta27@users.noreply.github.com>
2024-05-16 19:12:15 +0800

    lost file recovered

--- buwisbuddyph/app/build.gradle.kts
+++ buwisbuddyph/app/build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
     id("com.google.gms.google-services")
-    id("kotlin-kapt")
 }
 
 android {
@@ -82,8 +81,4 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-    implementation ("com.github.bumptech.glide:glide:4.13.2")
-    annotationProcessor ("com.github.bumptech.glide:compiler:4.13.2")
-    implementation ("com.google.android.material:material:1.4.0")
-
 }
--- buwisbuddyph/app/src/main/java/com/mab/buwisbuddyph/dataclass/ChatMessage.kt
+++
@@ -1,7 +0,0 @@
-package com.mab.buwisbuddyph.dataclass
-
-data class ChatMessage(
-    val senderId: String = "",
-    val message: String = "",
-    val timestamp: Long = System.currentTimeMillis()
-)
