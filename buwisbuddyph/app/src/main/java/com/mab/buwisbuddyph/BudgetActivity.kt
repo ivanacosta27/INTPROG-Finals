37e703a20b16c06a1ddd4899fdee623ea10745f5
Ivan Acosta <140309833+ivanacosta27@users.noreply.github.com>
2024-05-28 08:39:53 +0800

    all finished

--- buwisbuddyph/app/src/main/java/com/mab/buwisbuddyph/BudgetActivity.kt
+++ buwisbuddyph/app/src/main/java/com/mab/buwisbuddyph/BudgetActivity.kt
@@ -4,7 +4,9 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
@@ -24,6 +26,19 @@ class BudgetActivity : AppCompatActivity() {
         budgetET = findViewById(R.id.budget_ET)
         enterBudgetButton = findViewById(R.id.enterBudgetButton)
 
+        val returnIcon = findViewById<ImageView>(R.id.returnIcon)
+        returnIcon.setOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
+
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                val intent = Intent(this@BudgetActivity, HomeActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        })
+
         enterBudgetButton.setOnClickListener {
             val budgetStr = budgetET.text.toString()
             if (budgetStr.isNotEmpty()) {
@@ -40,8 +55,11 @@ class BudgetActivity : AppCompatActivity() {
     private fun saveBudgetToFirebase(budget: Double) {
         if (user != null) {
             val userRef = db.collection("users").document(user.uid)
-            val data = hashMapOf("userBudget" to budget, "userExpenseList" to emptyList<Map<String, String>>())
-            userRef.set(data)
+            val data = hashMapOf<String, Any>(
+                "userBudget" to budget,
+                "userExpenseList" to emptyList<Map<String, String>>()
+            )
+            userRef.update(data)
                 .addOnSuccessListener {
                     Toast.makeText(this, "Budget set and expenses cleared successfully", Toast.LENGTH_SHORT).show()
                 }
@@ -52,4 +70,5 @@ class BudgetActivity : AppCompatActivity() {
             Toast.makeText(this, "User not signed in", Toast.LENGTH_SHORT).show()
         }
     }
+
 }
--- buwisbuddyph/app/src/main/java/com/mab/buwisbuddyph/ProfileActivity.kt
+++ buwisbuddyph/app/src/main/java/com/mab/buwisbuddyph/ProfileActivity.kt
@@ -1,79 +1,245 @@
 package com.mab.buwisbuddyph
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
+import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
-import androidx.activity.addCallback
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import com.mab.buwisbuddyph.home.HomeActivity
-import com.squareup.picasso.Picasso
-import de.hdodenhof.circleimageview.CircleImageView
 
 class ProfileActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var db: FirebaseFirestore
+    private lateinit var storage: FirebaseStorage
+    private lateinit var userProfileImg: ImageView
+    private lateinit var storageRef: StorageReference
+    private var imageUri: Uri? = null
+    private var oldImageUri: Uri? = null // Variable to store the old image URI
+
+    companion object {
+        private const val TAG = "ProfileActivity"
+        private const val PICK_IMAGE_REQUEST = 1
+        private const val STORAGE_PERMISSION_CODE = 101
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
 
-        val auth = FirebaseAuth.getInstance()
-        val db = FirebaseFirestore.getInstance()
-        val storage = FirebaseStorage.getInstance()
+        auth = FirebaseAuth.getInstance()
+        db = FirebaseFirestore.getInstance()
+        storage = FirebaseStorage.getInstance()
+        storageRef = storage.reference
 
-        val currentUser = auth.currentUser
-
-        val userProfileImg: CircleImageView = findViewById(R.id.userProfileImg)
-        val nameTV: EditText = findViewById(R.id.nameTV)
-        val numberTV: EditText = findViewById(R.id.numberTV)
-        val genderTV: EditText = findViewById(R.id.genderTV)
-        val tinTV: EditText = findViewById(R.id.tinTV)
-        val emailTV: EditText = findViewById(R.id.emailTV)
+        userProfileImg = findViewById(R.id.userProfileImg)
+        userProfileImg.setOnClickListener {
+            if (isStoragePermissionGranted()) {
+                openGallery()
+            }
+        }
 
         val returnIcon = findViewById<ImageView>(R.id.returnIcon)
-        returnIcon.setOnClickListener{
+        returnIcon.setOnClickListener {
             onBackPressedDispatcher.onBackPressed()
         }
 
-        onBackPressedDispatcher.addCallback(this) {
-            val intent = Intent(this@ProfileActivity, HomeActivity::class.java)
-            startActivity(intent)
-            finish()
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                val intent = Intent(this@ProfileActivity, HomeActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        })
+
+        val currentUser = auth.currentUser
+        currentUser?.let {
+            loadUserProfile(it.uid)
         }
 
-        if (currentUser != null) {
-            db.collection("users").document(currentUser.uid)
-                .get()
-                .addOnSuccessListener { document ->
-                    if (document != null) {
-                        val name = document.getString("userFullName")
-                        val number = document.getString("userNumber")
-                        val gender = document.getString("userGender")
-                        val tin = document.getString("userTin")
-                        val email = document.getString("userEmail")
-                        val userProfileImgPath = document.getString("userProfileImg")
+        val editButton = findViewById<Button>(R.id.editButton)
+        val saveButton = findViewById<Button>(R.id.saveButton)
 
-                        name?.let { nameTV.setText(it) }
-                        number?.let { numberTV.setText(it) }
-                        gender?.let { genderTV.setText(it) }
-                        tin?.let { tinTV.setText(it) }
-                        email?.let { emailTV.setText(it) }
+        editButton.setOnClickListener {
+            setEditable(true)
+            saveButton.visibility = Button.VISIBLE
+        }
 
-                        if (userProfileImgPath != null) {
-                            val storageRef = storage.getReference(userProfileImgPath)
-                            storageRef.downloadUrl.addOnSuccessListener { uri ->
-                                Picasso.get().load(uri).into(userProfileImg)
-                            }.addOnFailureListener { exception ->
-                                Log.d("image","Error:",exception)
-                            }
-                        }
+        saveButton.setOnClickListener {
+            saveUserProfile()
+        }
+    }
+
+    private fun setEditable(isEditable: Boolean) {
+        findViewById<EditText>(R.id.nameTV)?.isEnabled = isEditable
+        findViewById<EditText>(R.id.numberTV)?.isEnabled = isEditable
+        findViewById<EditText>(R.id.genderTV)?.isEnabled = isEditable
+        findViewById<EditText>(R.id.tinTV)?.isEnabled = isEditable
+        findViewById<EditText>(R.id.emailTV)?.isEnabled = isEditable
+        Log.d("Profile Edit", "set all to editable")
+    }
+
+    private fun loadUserProfile(uid: String) {
+        db.collection("users").document(uid).get()
+            .addOnSuccessListener { document ->
+                if (document != null && document.exists()) {
+                    val name = document.getString("userFullName") ?: "N/A"
+                    val number = document.getString("userNumber") ?: "N/A"
+                    val gender = document.getString("userGender") ?: "N/A"
+                    val tin = document.getString("userTin") ?: "N/A"
+                    val email = document.getString("userEmail") ?: "N/A"
+                    val profileImage = document.getString("userProfileImage") // Retrieve the old image URI from Firestore
+
+                    // Store the old image URI if it exists
+                    oldImageUri = profileImage?.let { Uri.parse(it) }
+
+                    findViewById<EditText>(R.id.nameTV).setText(name)
+                    findViewById<EditText>(R.id.numberTV).setText(number)
+                    findViewById<EditText>(R.id.genderTV).setText(gender)
+                    findViewById<EditText>(R.id.tinTV).setText(tin)
+                    findViewById<EditText>(R.id.emailTV).setText(email)
+
+                    // Load the profile image
+                    // If profileImage is null, you can load a default image or leave it as is
+                    profileImage?.let { userProfileImg.setImageURI(oldImageUri) }
+                } else {
+                    Log.d(TAG, "No such document")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.d(TAG, "get failed with ", exception)
+            }
+    }
+
+    private fun saveUserProfile() {
+        val uid = auth.currentUser?.uid ?: return
+
+        val name = findViewById<EditText>(R.id.nameTV).text.toString()
+        val number = findViewById<EditText>(R.id.numberTV).text.toString()
+        val gender = findViewById<EditText>(R.id.genderTV).text.toString()
+        val tin = findViewById<EditText>(R.id.tinTV).text.toString()
+        val email = findViewById<EditText>(R.id.emailTV).text.toString()
+
+        // Only upload the image if a new image is selected
+        imageUri?.let { newImageUri ->
+            val imageRef = storageRef.child("userProfileImages/${uid}_profile_image")
+            val uploadTask = imageRef.putFile(newImageUri)
+
+            uploadTask            .continueWithTask { task ->
+                if (!task.isSuccessful) {
+                    task.exception?.let {
+                        throw it
                     }
                 }
+                imageRef.downloadUrl
+            }.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val downloadUri = task.result
+
+                    // Save the download URL to Firestore
+                    val userUpdates = mapOf(
+                        "userFullName" to name,
+                        "userNumber" to number,
+                        "userGender" to gender,
+                        "userTin" to tin,
+                        "userEmail" to email,
+                        "userProfileImage" to downloadUri.toString() // Add userProfileImage to Firestore
+                    )
+
+                    db.collection("users").document(uid)
+                        .update(userUpdates)
+                        .addOnSuccessListener {
+                            Log.d(TAG, "User profile updated successfully")
+                            // Make the EditText fields non-editable
+                            setEditable(false)
+                            // Hide the save button
+                            findViewById<Button>(R.id.saveButton).visibility = Button.GONE
+                        }
+                        .addOnFailureListener { exception ->
+                            Log.d(TAG, "Error updating user profile", exception)
+                        }
+                } else {
+                    Log.d(TAG, "Error uploading image: ${task.exception}")
+                }
+            }
+        } ?: run {
+            // If no new image is selected, update other user profile fields without changing the profile image
+            val userUpdates = mapOf(
+                "userFullName" to name,
+                "userNumber" to number,
+                "userGender" to gender,
+                "userTin" to tin,
+                "userEmail" to email
+            )
+
+            db.collection("users").document(uid)
+                .update(userUpdates)
+                .addOnSuccessListener {
+                    Log.d(TAG, "User profile updated successfully")
+                    // Make the EditText fields non-editable
+                    setEditable(false)
+                    // Hide the save button
+                    findViewById<Button>(R.id.saveButton).visibility = Button.GONE
+                }
                 .addOnFailureListener { exception ->
-                    Log.d("profile","Error:",exception)
+                    Log.d(TAG, "Error updating user profile", exception)
                 }
         }
     }
+
+    private fun openGallery() {
+        val galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(galleryIntent, PICK_IMAGE_REQUEST)
+    }
+
+    private fun isStoragePermissionGranted(): Boolean {
+        return if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+            == PackageManager.PERMISSION_GRANTED
+        ) {
+            true
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                STORAGE_PERMISSION_CODE
+            )
+            false
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == STORAGE_PERMISSION_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                openGallery()
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.data != null) {
+            imageUri = data.data!!
+            // Update the profile image here with the selected imageUri
+            userProfileImg.setImageURI(imageUri)
+        }
+    }
 }
+
